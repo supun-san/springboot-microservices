4b374f108c87843071edfffc7da5992f2a3adcc4
Supunsan <supun-san@users.noreply.github.com>
2025-03-02 11:04:28 +0530

    Fix: Added missing spring-cloud-starter-gateway dependency

--- school/src/main/java/com/san/school/entity/School.java
+++ school/src/main/java/com/san/school/entity/School.java
@@ -2,6 +2,7 @@ package com.san.school.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
@@ -14,7 +15,7 @@ import lombok.*;
 public class School {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
--- student/src/main/java/com/san/student/entity/Student.java
+++ student/src/main/java/com/san/student/entity/Student.java
@@ -2,6 +2,7 @@ package com.san.student.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
@@ -14,7 +15,7 @@ import lombok.*;
 public class Student {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String firstName;
     private String lastName;
